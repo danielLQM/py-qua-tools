5dcaab53a273b61c4155e7ed8f853b6d58307e7b
Kevin Villegas <kevin@quantum-machines.co>
2021-12-22 16:23:41 -0500

    Added waveform_tools after Yoav's

--- qualang_tools/config/waveform_tools.py
+++ qualang_tools/config/waveform_tools.py
@@ -1,24 +1,45 @@
 import numpy as np
 
-# Definition of pulses follow Chen et al. PRL, 116, 020501 (2016)
+"""
+Example of parameters and how the waveforms can be defined:
+
+drag_len = 16  # length of pulse in ns
+drag_amp = 0.1  # amplitude of pulse in Volts
+drag_del_f = - 0e6  # Detuning frequency in Hz
+drag_alpha = 1  # DRAG coefficient
+drag_delta = 2 * np.pi * (- 200e6 - drag_del_f)  # in Hz
+
+Gaussian envelope:
+drag_gauss_I_wf, drag_gauss_Q_wf = drag_gaussian_pulse_waveforms(drag_amp, drag_len, drag_len / 5, drag_alpha, drag_delta, drag_del_f, substracted=False)  # pi pulse
+
+Cosine envelope:
+drag_cos_I_wf, drag_cos_Q_wf = drag_cosine_pulse_waveforms(drag_amp, drag_len, drag_alpha, drag_delta, drag_del_f)  # pi pulse
+
+"""
 
 
 def drag_gaussian_pulse_waveforms(
-    amplitude, length, sigma, alpha, detune, delta, substracted
+    amplitude, length, sigma, alpha, delta, detune=0, substracted=True
 ):
     """
     Function that returns DRAG pulses that compensate for leakage and also for the AC stark shift
     that the leakage creates to the qubit frequency.
 
-    Envelope: gaussian
+    These DRAG waveforms has been implemented following the next Refs.:
+    Chen et al. PRL, 116, 020501 (2016)
+    https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.020501
+    and Chen's thesis
+    https://web.physics.ucsb.edu/~martinisgroup/theses/Chen2018.pdf
 
-    :param amplitude: amplitude of voltage
-    :param length: pulse length
-    :param sigma: gaussian standard deviation
-    :param alpha: DRAG coefficient
-    :param detune: frequency shift to correct for AC start shift
-    :param delta: f_21 - f_10 - detune
-    :param substracted: if you would like to use a substracted gaussian
+    Envelope type: gaussian
+
+    :param float amplitude: amplitude in volts
+    :param int length: pulse length in ns
+    :param float sigma: gaussian standard deviation
+    :param float alpha: DRAG coefficient
+    :param float detune: frequency shift to correct for AC start shift
+    :param float delta: f_21 - f_10 - detune
+    :param bool substracted: if you would like to use a substracted gaussian
     """
     t = np.arange(length, dtype=int)  # array of size pulse length in ns
     gauss_wave = amplitude * np.exp(
@@ -38,18 +59,24 @@ def drag_gaussian_pulse_waveforms(
     return I_wf, Q_wf
 
 
-def drag_cosine_pulse_waveforms(amplitude, length, alpha, detune, delta):
+def drag_cosine_pulse_waveforms(amplitude, length, alpha, delta, detune=0):
     """
     Function that returns DRAG pulses that compensate for leakage and also for the AC stark shift
     that the leakage creates to the qubit frequency.
 
-    Envelope: cosine
+    These DRAG waveforms has been implemented following the next Refs.:
+    Chen et al. PRL, 116, 020501 (2016)
+    https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.020501
+    and Chen's thesis
+    https://web.physics.ucsb.edu/~martinisgroup/theses/Chen2018.pdf
 
-    :param amplitude: amplitude of voltage
-    :param length: pulse length
-    :param alpha: DRAG coefficient
-    :param detune: frequency shift to correct for AC start shift
-    :param delta: f_21 - f_10 - detune
+    Envelope type: cosine
+
+    :param float amplitude: amplitude of voltage
+    :param int length: pulse length
+    :param float alpha: DRAG coefficient
+    :param float detune: frequency shift to correct for AC start shift
+    :param float delta: f_21 - f_10 - detune
     """
     t = np.arange(length, dtype=int)  # array of size pulse length in ns
     cos_wave = 0.5 * amplitude * (1 - np.cos(t * 2 * np.pi / length))  # cosine function
